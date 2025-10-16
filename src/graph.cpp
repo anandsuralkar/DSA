5b450bf3bb9ff4aee890047a9823bacb2b1dc989
anand <anandsuralkar36@gmail.com>
2025-10-16 23:26:02 +0530

    added topology sort and safenodes

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,6 +2,10 @@
 #include<vector>
 #include<queue>
 #include<sstream>
+#include<map>
+#include<unordered_map>
+#include<stack>
+#include<algorithm>
 using namespace std;
 vector<vector<int>> Grapher(const std::string& input);
 vector<vector<int>> graph();
@@ -35,7 +39,7 @@ void print(const vector<vector<int>> &graph){
         for (int j=0;j<(int)graph[i].size();j++)cout<<graph[i][j]<<" ";cout<<endl;
     }
 }
-void bfs_connected(vector<vector<int>> &G, int start){
+void bfs_connected(const vector<vector<int>> &G, int start){
     int nodes = G.size();
     vector<bool> visited(nodes,false);
     queue<int> q;
@@ -54,7 +58,7 @@ void bfs_connected(vector<vector<int>> &G, int start){
         }
     }cout<<endl;
 }
-void dfs_connected_(vector<vector<int>> &G, vector<int> &traversal, vector<bool> &visited, int node){
+void dfs_connected_(const vector<vector<int>> &G, vector<int> &traversal, vector<bool> &visited, int node){
     if(!visited[node]){
         visited[node]=true;
         traversal.push_back(node);
@@ -62,7 +66,7 @@ void dfs_connected_(vector<vector<int>> &G, vector<int> &traversal, vector<bool>
         dfs_connected_(G, traversal, visited, child);
     }
 }
-void dfs_connected(vector<vector<int>> &G, int start){
+void dfs_connected(const vector<vector<int>> &G, int start){
     int size = G.size();
     vector<bool> visited(size,false);
     vector<int> traversal;
@@ -70,7 +74,7 @@ void dfs_connected(vector<vector<int>> &G, int start){
     dfs_connected_(G, traversal, visited, start);
     for(int node: traversal)cout<<node<<" ";cout<<endl;
 }
-void reg_(vector<vector<int>> &G, vector<bool> &visited, int node){
+void reg_(const vector<vector<int>> &G, vector<bool> &visited, int node){
     if (!visited[node]){
         visited[node]=true;
         for(auto current:G[node]){
@@ -78,7 +82,7 @@ void reg_(vector<vector<int>> &G, vector<bool> &visited, int node){
         }
     }
 }
-void regions(vector<vector<int>> &G){
+void regions(const vector<vector<int>> &G){
     int size = G.size();
     vector<bool> visited(size,false);
     int count=0;
@@ -90,7 +94,7 @@ void regions(vector<vector<int>> &G){
     }
     cout<<"Regions: "<<count<<endl;
 }
-void bfs(vector<vector<int>> &G){
+void bfs(const vector<vector<int>> &G){
     int size= G.size();
     vector<bool> visited(size, false);
     queue<int> q;
@@ -116,7 +120,7 @@ void bfs(vector<vector<int>> &G){
         }
     }
 }
-void dfs_(vector<vector<int>> &G, vector<bool> &visited, int node){
+void dfs_(const vector<vector<int>> &G, vector<bool> &visited, int node){
     visited[node]=true;
     cout<<node<<" ";
     for(int neighbour: G[node]){
@@ -125,7 +129,7 @@ void dfs_(vector<vector<int>> &G, vector<bool> &visited, int node){
         }
     }
 }
-void dfs(vector<vector<int>> &G){
+void dfs(const vector<vector<int>> &G){
     int size = G.size();
     int count =0;
     cout<<"DFS ->"<<endl;
@@ -140,7 +144,7 @@ void dfs(vector<vector<int>> &G){
     }
 }
 
-bool cycle_bfs_(vector<vector<int>> &G, int start){
+bool cycle_bfs_(const vector<vector<int>> &G, int start){
     vector<bool> visited(G.size(),false);
     queue<pair<int,int>> q;
     q.push({start,-1});
@@ -162,7 +166,7 @@ bool cycle_bfs_(vector<vector<int>> &G, int start){
     }
     return false;
 }
-bool detect_bfs(vector<vector<int>> &G,vector<bool> &visited,int start){
+bool detect_bfs(const vector<vector<int>> &G,vector<bool> &visited,int start){
     queue<pair<int,int>> q;
     q.push({start,-1});
     cout<<"{";
@@ -185,7 +189,7 @@ bool detect_bfs(vector<vector<int>> &G,vector<bool> &visited,int start){
     cout<<"} : ";
     return iscycle;
 }
-void cycle_bfs(vector<vector<int>> &G){
+void cycle_bfs(const vector<vector<int>> &G){
     vector<bool> visited(G.size(),false);
     for (int i=0;i<visited.size();i++){
         if(!visited[i]){
@@ -193,7 +197,7 @@ void cycle_bfs(vector<vector<int>> &G){
         }
     }
 }
-bool detect_dfs(vector<vector<int>>&G,vector<bool>&visited,vector<int> &trace,int start,int parent){
+bool detect_dfs(const vector<vector<int>>&G,vector<bool>&visited,vector<int> &trace,int start,int parent){
     bool iscycle = false;
     if(!visited[start]){
         visited[start]=true;
@@ -209,7 +213,7 @@ bool detect_dfs(vector<vector<int>>&G,vector<bool>&visited,vector<int> &trace,in
     }
     return iscycle;
 }
-void cycle_dfs(vector<vector<int>> &G){
+void cycle_dfs(const vector<vector<int>> &G){
     vector<bool> visited(G.size(),false);
     for(int i=0;i<G.size();i++){
         if(!visited[i]){
@@ -223,18 +227,168 @@ void cycle_dfs(vector<vector<int>> &G){
         }
     }
 }
+bool dfs_b(const vector<vector<int>>&G,vector<int> &colors,int start,int color){
+    bool isBipartite = true;
+    colors[start] = color;
+    cout<<start<<"->";
+    if(G[start].size()>0){
+        for(int node : G[start]){
+            if(color == colors[node]){
+                isBipartite = false;
+            }
+            if(colors[node]==-1){
+                if(!dfs_b(G,colors,node,!color)){
+                    isBipartite = false;
+                }
+            }
+        }
+    }
+    return isBipartite;
+}
+bool bipartite(const vector<vector<int>> &G){
+    bool isBipartite = true;
+    vector<int> colors(G.size(),-1);
+    int count = 0;
+    for(int i=0;i<G.size();i++){
+        if(colors[i]==-1){
+            count++;
+            cout<<"SG "<<count<<": ";
+            if(!dfs_b(G,colors,i,1)){
+                isBipartite = false;
+                cout<<"=> false;";
+            }
+            else{
+                cout<<"=> true";
+            }
+            cout<<endl;
+        }
+    }
+    return isBipartite;
+}
+
+bool bfs_b(const vector<vector<int>>&G, vector<int> &colors, int start,int color){
+    queue<int> q;
+    bool isBipartite = true;
+    q.push(start);
+    colors[start]=color;
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        cout<<curr<<"-"<<(colors[curr]?"Red":"Blue")<<" ";
+        for(int node : G[curr]){
+            if(colors[node]==colors[curr]){
+                isBipartite = false;
+            }
+            if(colors[node]==-1){
+                q.push(node);
+                colors[node] = !colors[curr];
+            }
+        }
+    }
+    return isBipartite;
+}
+bool bipartite_bfs(const vector<vector<int>> &G){
+    vector<int> colors(G.size(),-1);
+    bool isBipartite = true;
+    int count = 0;
+    for(int i=0;i<G.size();i++){
+        if(colors[i] == -1){
+            count++;
+            cout<<"SubGraph "<<count<<"{";
+            isBipartite = bfs_b(G,colors,i,1);
+            cout<<"}: "<<(isBipartite? "true":"false")<<endl;
+
+        }
+    }
+    return isBipartite;
+}
+void dfs_topo(const vector<vector<int>>&G,vector<bool>&visited,stack<int> &s,int node){
+    visited[node]=true;
+    for(int neighbor: G[node]){
+        if(!visited[neighbor]){
+            dfs_topo(G,visited,s,neighbor);
+        }
+    }
+    s.push(node);
+
+}
+vector<int> topology(const vector<vector<int>> &G){
+    int size = G.size();
+    vector<bool> visited(size,false);
+    vector<int> out;
+    stack<int> s;
+    for(int i=0;i<size;i++){
+        if(!visited[i]){
+            dfs_topo(G,visited,s,i);
+        }
+    }
+    for(int j=0;j<size;j++){
+        out.push_back(s.top());
+        s.pop();
+    }
+    return out;
+}
+vector<int> safesates(const vector<vector<int>>&G){
+    int size = G.size();
+    vector<int> indegree(size,0);
+    queue <int>q;
+    vector<vector<int>> rev(size);
+    vector<int>safenodes;
+    for(int i=0;i<size;i++){
+        for(auto it: G[i]){
+            rev[it].push_back(i);
+            indegree[i]++;
+        }
+    }
+    for(int k=0;k<size;k++){
+        if(indegree[k]==0){
+            q.push(k);
+        }
+    }
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        safenodes.push_back(curr);
+        for(auto it: rev[curr]){
+            indegree[it]--;
+            if(indegree[it]==0){
+                q.push(it);
+            }
+        }
+    }
+    sort(safenodes.begin(),safenodes.end());
+    return safenodes;
+}
 int main(){
     string input = "20 2 1 4 3 0 2 3 1 1 1 1 3 0 5 7 2 4 6 2 5 7 2 4 6 1 9 2 8 10 1 9 2 12 13 2 11 14 2 11 14 2 12 13 2 16 17 2 15 17 3 15 16 18 1 17 0";
-
-    vector<vector<int>>G = grapher(input);
+    string inp = "8 3 4 5 6 2 4 7 1 5 2 6 7 2 0 1 2 0 2 0 1 0 1 2 3";
+    string top="6 2 3 4 1 0 1 0 0 0 2 3 4";
+    string safe="6 1 3 2 0 4 1 0 0 1 5 2 3 4";
+/*
+    const vector<vector<int>>G = grapher(input);
     print(G);
     bfs_connected(G,0);
     dfs_connected(G,0);
     bfs(G);
     dfs(G);
     regions(G);
-    cout<<"Cycle: "<<cycle_bfs_(G,8)<<endl;
+    cout<<"Cycle: "<<cycle_bfs_(G,3)<<endl;
     cycle_bfs(G);
     cycle_dfs(G);
+    cout<<(bipartite(G)? "Bipartite":"Not bipartite")<<endl;
+    bipartite_bfs(G);
+    const vector<vector<int>>Gs = grapher(top);
+    print(Gs);
+    vector<int> ans = topology(Gs);
+    cout<<"Topological Sort: ";
+    for(int node: ans)cout<<node<<" ";cout<<endl;
+*/
+    const vector<vector<int>>Gs = grapher(safe);
+    print(Gs);
+    const vector<int> ans = safesates(Gs);
+    cout<<"safesates : ";
+    for(int node: ans)cout<<node<<" ";cout<<endl;
     return 0;
+
 }
+//whats up my friend whar are  you doing my frined are u okay brother whats up bro get some sleep my friend why are u doing okay
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<set>
 using namespace std;
 void print(vector<vector<int>> &M,int color){
     for(int k=0;k<20;k++)cout<<"_";cout<<endl;
@@ -61,7 +62,112 @@ vector<vector<int>> nearest1(vector<vector<int>> &G) {
 
     return dist;
 }
+void bfs(vector<vector<int>> &G,vector<vector<bool>> & visited,int x,int y){
+    queue<pair<int,int>> q;
+    vector<pair<int,int>> group;
+    int sizex = G.size();
+    int sizey = G[0].size();
+    bool isSurrounded=true;
+    q.push({x,y});
+    int dx[] = {-1,1,0,0};
+    int dy[] = {0,0,-1,1};
+    while(!q.empty()){
+        auto [a,b] = q.front();
+        q.pop();
+        if(!visited[a][b]){
+            visited[a][b] =true;
+            if(G[a][b]==0){
+                group.push_back({a,b});
+                if(a == 0 || a == sizex-1 || b == 0 || b == sizey-1){
+                    isSurrounded = false;
+                }
+                for(int dir=0;dir<4;dir++){
+                    int xx =a+dx[dir];
+                    int yy =b+dy[dir];
+                    if(xx>=0 && xx<sizex && yy>=0 && yy<sizey){
+                        if(G[xx][yy]==0){
+                            q.push({xx,yy});
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (isSurrounded){
+        for( auto[x,y] : group){
+            G[x][y] = 1;
+        }
+    }
+}
+vector<vector<int>> surround(vector<vector<int>> &G){
+    //O is 0
+    //X is 1
+    int sizex = G.size();
+    int sizey = G[0].size();
+    vector<vector<int>> M(sizex,vector<int>(sizey,0));
+    vector<vector<bool>> visited (sizex,vector<bool>(sizey,false));
+    for(int i = 0;i<sizex;i++){
+        for(int j=0;j<sizey;j++){
+            M[i][j]=G[i][j];
+        }
+    }
+    for(int i=0;i<sizex;i++){
+        for(int j=0;j<sizey;j++){
+            if(!visited[i][j] && M[i][j]==0){
+                bfs(M,visited,i,j);
+            }
+        }
+    }
+    return M;
+}
+vector<pair<int,int>> bfs_distinct(vector<vector<int>>&G,vector<vector<bool>>&visited,pair<int,int> start){
+    vector<int> group;
+    queue<pair<int,int>> q;
+    vector<pair<int,int>> trace;
+    int dx[] = {-1,1,0,0};
+    int dy[] = {0,0,-1,1};
+    int sizex = G.size();
+    int sizey = G[0].size();
+    q.push(start);
+    while(!q.empty()){
+        auto [x,y] = q.front();
+        q.pop();
+        if(!visited[x][y]){
+            visited[x][y]=true;
+            trace.push_back({x - start.first ,y - start.second});
+            for(int dir=0;dir<4;dir++){
+                int xx = x+dx[dir];
+                int yy = y+dy[dir];
+                if(xx>=0 && xx<sizex && yy>=0 && yy<sizey && !visited[xx][yy] && G[xx][yy]==1){
+                    q.push({xx,yy});    
+                }
+            }
+        }
+    }
+    return trace;
 
+}
+int islands(vector<vector<int>> &G){
+    const int sizex = G.size();
+    const int sizey = G[0].size();
+    set<vector<pair<int,int>>> islands;
+    vector<vector<bool>> visited(G.size(),vector<bool>(G[0].size(),false));
+    for(int i=0;i<sizex;i++){
+        for(int j=0;j<sizey;j++){
+            if(!visited[i][j] && G[i][j]==1){
+                islands.insert(bfs_distinct(G,visited,{i,j}));
+            }
+        }
+    }
+    for(auto island :islands){
+        cout<<"[";
+        for(auto[one,two]:island){
+            cout<<"("<<one<<","<<two<<")"<<", ";
+        }
+        cout<<"]"<<endl;
+    }
+    return islands.size();
+}
 int main(){
     vector<vector<int>> M = {
         {2,2,2,2,2,2,2,0,2,2},
@@ -76,17 +182,25 @@ int main(){
         {2,0,0,2,2,2,2,2,2,2}
     };
     vector<vector<int>> N = {
-        {0,0,0,0,0,1,0,0,0,0},
-        {0,0,0,1,1,1,1,0,1,0},
-        {0,1,0,0,0,0,0,0,1,0},
-        {0,1,0,0,1,1,1,0,1,0},
+        {0,0,0,0,0,1,0,1,0,0},
+        {0,0,0,1,0,1,1,0,1,0},
+        {0,1,0,0,1,0,0,0,1,0},
+        {0,1,1,0,1,1,1,0,1,0},
         {0,1,0,1,0,0,1,0,1,0},
-        {0,1,0,0,1,0,1,0,0,0},
-        {1,1,0,0,0,0,0,0,1,0},
-        {0,1,0,0,0,0,0,0,1,0},
+        {0,1,0,0,1,0,1,0,0,1},
+        {1,1,0,1,0,0,1,0,1,0},
+        {0,1,1,0,0,0,1,0,1,0},
         {0,0,0,0,1,0,0,1,1,0},
         {1,0,0,1,0,0,0,0,0,0}
     };
+    vector<vector<int>> C={
+        {0,1,0,1,1},
+        {1,1,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,1,1},
+        {1,1,0,1,0},
+    };
+
     // cout<<"__________________Original_Canvas__________________"<<endl;
     // print(M,-1);
     // cout<<"____________0_pixelx_in_Original_Canvas____________"<<endl;
@@ -98,7 +212,15 @@ int main(){
     // print(M,0);
     // cout<<"_________________After_floodfill___________________"<<endl;
     // print(M,-1);
+
+
+
+    //print(N,-1);
+    //vector<vector<int>> O = nearest1(N);
+    //print(O,-1);
     print(N,-1);
-    vector<vector<int>> O = nearest1(N);
-    print(O,-1);
+    vector<vector<int>> S = surround(N);
+    print(S,-1);
+    print(C,-1);
+    cout<<islands(C)<<endl;
 }
